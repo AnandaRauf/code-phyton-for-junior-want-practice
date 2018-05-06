11d978ff2bb9ee2af57e1785ebb5817f36d6ad74
AnandaRauf <uzumaki.nanda@rocketmail.com>
2018-05-06 08:31:10 +0700

    versi 1.1.1

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# < for Commentar ( free Change code whatever you)
+print < for show text in cmd
+\n< for Paragrapgh Text
+upper()< for BIG Alfabet
+lower()<for SMALL Alfabet
+len< for leght Word/String
+str() + for String and add string 
+To Be Continued, You Can See My Code ^^
+Thanks ^^
---
+++ ngantuk.py
@@ -0,0 +1,14 @@
+#""" Aing Ngantuk
+# Author: Ananda Rauf Maududi
+# Nama Program yang akan dibuat : ngantuk.py"""
+kata= " Ngantuk berat bray kalo mau jadi Programmer Senior\n "
+nasihat= "Kuatkan fisikmu dan otakmu terlebih dahulu, jika mau jadi Programmer Senior\n "
+ucapan= "Sampai jumpa lagi dengan saya\n " "Miss you ^^\n "
+# panjang ucapan simpan di sini dengan len()
+panjang= len(kata)
+# hasilkan huruf kapital dengan upper()
+besar=  kata.upper()
+# hasilkan huruf kecil semua dengan lower()
+kecil = kata.lower()
+# Jangan lupa kalo mau lihat hasilnya pakai print
+print str(kata) + str(nasihat).upper() + str(ucapan)
